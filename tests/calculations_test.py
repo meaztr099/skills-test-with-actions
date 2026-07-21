8e56bef67699acbf962c9a80acd03b72052479ef
meaztr099 <102753786+meaztr099@users.noreply.github.com>
2026-07-21 12:31:32 +0000

    Reenable unit test that was disabled

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -59,13 +59,13 @@ def test_get_nth_fibonacci_one():
 
 
 def test_get_nth_fibonacci_ten():
-     """Test with n=11."""
+     """Test with n=10."""
      # Arrange
-     n = 11
+     n = 10
 
      # Act
      result = get_nth_fibonacci(n)
 
 
      # Assert
-     assert result == 89
+     assert result == 55
